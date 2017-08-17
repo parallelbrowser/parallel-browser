0d490c7517950cc4e79ac6d175dba0b78561fa9c
danhuiZ <danhuizzz@gmail.com>
2017-08-16 22:29:35 -0700

    comment out extra file

--- app/lib/web-apis/parallel.js
+++ app/lib/web-apis/parallel.js
@@ -1,12 +1,12 @@
-import ParallelAPI from 'parallel-scratch-api'
-import DatArchive from './dat-archive'
-window.DatArchive = DatArchive
-
-export default {
-  async loadPrescripts () {
-    const userURL = 'dat://9349f5ff0c0a05332d9002eefc82d8ca935b9bc58e3be8e6967eb29b9615b491'
-    const userDB = await ParallelAPI.open(new DatArchive(userURL))
-    console.log('userDB', userDB)
-    this.prescripts = await userDB.listPrescripts()
-  }
-}
+// import ParallelAPI from 'parallel-scratch-api'
+// import DatArchive from './dat-archive'
+// window.DatArchive = DatArchive
+//
+// export default {
+//   async loadPrescripts () {
+//     const userURL = 'dat://9349f5ff0c0a05332d9002eefc82d8ca935b9bc58e3be8e6967eb29b9615b491'
+//     const userDB = await ParallelAPI.open(new DatArchive(userURL))
+//     console.log('userDB', userDB)
+//     this.prescripts = await userDB.listPrescripts()
+//   }
+// }
